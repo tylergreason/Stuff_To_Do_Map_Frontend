3de22bb9473a146bbee6e32a7e1f67b7456a5821
tylergreason <tyler.greason@protonmail.com>
2020-04-01 16:40:21 -0400

    added redux, thunk. Created empty store. Moving to create routes."

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware} from 'redux' 
+import mapReducer from './reducers/mapReducer'
+import thunk from 'redux-thunk'
+
+ 
+const store = createStore(mapReducer, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/mapReducer.js
@@ -0,0 +1,7 @@
+const mapReducer = (state = {}, action) => {
+    switch(action.type){
+        default: 
+            return state
+    }
+}
+export default mapReducer
