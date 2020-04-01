9d930587829d7e505b1ac2397b480ff31f65627e
tylergreason <tyler.greason@protonmail.com>
2020-04-01 16:53:39 -0400

    added login/logout functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'; 
+
+class App extends Component {
+  state = {
+
+  }
+
+
+  handleLogin = () => {
+    this.setState({
+      loggedIn:true
+    })
+  }
+
+  handleLogout = () => {
+    localStorage.clear() 
+    this.setState({
+      loggedIn:false 
+    })
+  }
+  render(){
+
+    return (
+      <div className="App">
+      App Component 
     </div>
   );
+}
 }
 
 export default App;
