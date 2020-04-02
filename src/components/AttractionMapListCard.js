30fcbf1e5f8bdb72ddb878429a688bc8730a96df
tylergreason <tyler.greason@protonmail.com>
2020-04-02 13:11:49 -0400

    attractions listed. Moving on to rendering map

---
+++ src/components/AttractionMapListCard.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+const AttractionMapListCard = (props) => {
+    console.log(props.attraction)
+    return (
+        <div>
+            <h4>{props.attraction.name}</h4>
+            {`${props.attraction.house_number} ${props.attraction.road}, ${props.attraction.city}, ${props.attraction.state}, ${props.attraction.country}`}
+            <br></br>
+            {props.attraction.description}
+        </div>)
+}
+
+export default AttractionMapListCard
--- src/containers/AttractionList.js
+++ src/containers/AttractionList.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react' 
 import { connect } from 'react-redux'
 import { getAttractions } from '../actions/MapActions'
+import AttractionMapListCard from '../components/AttractionMapListCard'
 
 class AttractionList extends Component {   
     renderAttractionCards = () => {
-        
+        return this.props.attractions.map(attraction => {
+            console.log(attraction)
+            return <AttractionMapListCard attraction={attraction}/> 
+        })
     }
     render(){
-        return(<>attraction list</>)
+        return(<>
+            <h3>
+                attraction list
+            </h3>
+            {this.renderAttractionCards()}
+        </>)
     }
 }
 
